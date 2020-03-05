8ad336d584c096a5b238aa0987b18f6b232922e6
blackMoja <limlim980625@gmail.com>
2020-03-05 22:37:37 +0900

    [개발]  - 코드 고도화 및 소스 정리  - constructor maximimsize args로 받게 수정

--- src/js/resize-image.js
+++ src/js/resize-image.js
@@ -8,39 +8,32 @@
  * @returns {Files} 리사이징 파일 || 원본 파일 return 
  */
 
-// class 구조로 변경 
-// async awit 
+// test src : https://apis.iparking.co.kr/iparking-incoming-images/garage/apply/hyundai/2020-02-20/1582187877141_20200125_200239.jpg
 export default class Resize {
-  constructor(target, size, maximumSize) {
-    this.target = target;
-    this.size = size;
+  constructor(maximumSize) {
     this.maximumSize = maximumSize;
   }
 
-  resize() {
-    return isFile() ? byPass() : convertUrl();
+  resize(target, size) {
+    // convertUrl이 독자적으로 움직일 수 있게끔 조치가 필요함.
+    return this.isFile() ? this.byPass(target, size) : this.convertUrl();
   }
 
   isFile() {
     return this.target instanceof File;
   }
 
-  async byPass() {
-    return await byPass() ? this.target : this.convertFile();
+  async byPass(target, size) {
+    return await this.checkSize(target) ? this.target : this.convertFile(target, size);
   }
 
-  checkSize() {
+  checkSize(target) {
     return new Promise(resolve => {
-      return resolve(this.maximumSize > this.target.size);
+      return resolve(this.maximumSize > target);
     });
   }
 
-  async convertFile() {
-    const dataUrl = await this.fileToDataurl();
-    await this.convertUrl(dataUrl);
-  }
-
-  fileToDataurl() {
+  fileToDataurl(target) {
     return new Promise(resolve => {
       const fr = new FileReader();
 
@@ -51,22 +44,29 @@ export default class Resize {
     });
   }
 
-  convertUrl(parseUrl) {
+  async convertFile(target, size) {
+    const dataUrl = await this.fileToDataurl(target);
+    const convertUrl = await this.convertUrl(dataUrl, size, target.size);
+
+    return this.urlToBlob(convertUrl, target.name);
+  }
+
+  convertUrl(parseUrl, size, originSize) {
     return new Promise(resolve => {
-      const canvas = document.createElement('canvas');
+      let canvas = document.createElement('canvas');
       let img = new Image();
 
       img.src = parseUrl;
       img.crossOrigin = 'anonymous';
 
       img.onload = () => {
+        canvas.width = size.width || 600;
+        canvas.height = size.height || 600;
 
-        canvas.width = this.size.width || 600;
-        canvas.height = this.size.height || 600;
+        canvas.getContext('2d').drawImage(img, 0, 0, size.width, size.height);
 
-        canvas.getContext('2d').drawImage(img, 0, 0, this.size.width, this.size.height);
-
-        const origin = Math.min(this.target.size, 300 * 1024);
+        // !리펙토링 시작 - 20.02.29
+        const origin = Math.min(originSize, 300 * 1024);
         let q = 0.5;
         let d = 0.5;
         let dataUrl = '';
@@ -88,9 +88,9 @@ export default class Resize {
     });
   }
 
-  urlToBlob() {
-    const bytes = this.target.split(',')[0].indexOf('base64') >= 0 ? atob(this.target.split(',')[1]) : encodeURI(this.target.split(',')[1]);
-    const mime = this.target.split(',')[0].split(':')[1].split(';')[0];
+  urlToBlob(result, fileName) {
+    const bytes = result.split(',')[0].indexOf('base64') >= 0 ? atob(result.split(',')[1]) : encodeURI(result.split(',')[1]);
+    const mime = result.split(',')[0].split(':')[1].split(';')[0];
     const max = bytes.length;
     const ia = new Uint8Array(max);
 
@@ -98,6 +98,6 @@ export default class Resize {
       ia[i] = bytes.charCodeAt(i);
     }
 
-    return new File([new Blob([ia], { type: mime })], this.target.name);
+    return new File([new Blob([ia], { type: mime })], fileName);
   }
 };
