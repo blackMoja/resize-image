331e89c238bb8da2a2e043c5db708cc80e3a77b8
cmlim <limlim980625@gmail.com>
2020-03-20 11:56:58 +0900

    [개발]  - file type 추가

--- src/js/resize-image.js
+++ src/js/resize-image.js
@@ -1,11 +1,11 @@
-/**  
- * resize  
- * 리사이즈  
- * @param {*} target File or image Url  
- * @param {*} size { width: 600, height: 600 } 
- * @param {*} maximumSize 최대 maximumsize 
- *  
- * @returns {Files} 리사이징 파일 || 원본 파일 return 
+/**
+ * resize
+ * 리사이즈
+ * @param {*} target File or image Url
+ * @param {*} size { width: 600, height: 600 }
+ * @param {*} maximumSize 최대 maximumsize
+ *
+ * @returns {Files} 리사이징 파일 || 원본 파일 return
  */
 export default class Resize {
   constructor(maximumSize) {
@@ -13,7 +13,9 @@ export default class Resize {
   }
 
   resize(target, size) {
-    return this.isFile(target) ? this.byPass(target, size) : this.parseUrl(target, size);
+    return this.isFile(target)
+      ? this.byPass(target, size)
+      : this.parseUrl(target, size);
   }
 
   isFile(target) {
@@ -21,7 +23,9 @@ export default class Resize {
   }
 
   async byPass(target, size) {
-    return await this.checkSize(target) ? target : this.convertFile(target, size);
+    return (await this.checkSize(target))
+      ? target
+      : this.convertFile(target, size);
   }
 
   checkSize(target) {
@@ -36,7 +40,7 @@ export default class Resize {
 
       fr.onload = e => {
         return resolve(e.target.result);
-      }
+      };
       fr.readAsDataURL(target);
     });
   }
@@ -50,33 +54,35 @@ export default class Resize {
 
   async parseUrl(target, size) {
     const convertUrl = await this.convertUrl(target, size);
-    return this.urlToBlob(convertUrl, 'download.jpg');
+    return this.urlToBlob(convertUrl, "download.jpg");
   }
 
   convertUrl(parseUrl, size, originSize) {
     return new Promise(resolve => {
-      let canvas = document.createElement('canvas');
+      let canvas = document.createElement("canvas");
       let img = new Image();
 
       img.src = parseUrl;
-      img.crossOrigin = 'anonymous';
+      img.crossOrigin = "anonymous";
 
       img.onload = () => {
         canvas.width = size.width || 600;
         canvas.height = size.height || 600;
 
-        canvas.getContext('2d').drawImage(img, 0, 0, size.width, size.height);
+        canvas.getContext("2d").drawImage(img, 0, 0, size.width, size.height);
 
         // url 일 경우... 300 * 1024 고정이 맞는걸까 ?
-        const origin = originSize ? Math.min(originSize, 300 * 1024) : 300 * 1024;
+        const origin = originSize
+          ? Math.min(originSize, 300 * 1024)
+          : 300 * 1024;
         let q = 0.5;
         let d = 0.5;
-        let dataUrl = '';
-        let encUrl = '';
+        let dataUrl = "";
+        let encUrl = "";
 
         for (let i = 0; i < 7; i++) {
-          dataUrl = canvas.toDataURL('image/jpeg', q);
-          encUrl = atob(dataUrl.replace('data:image/jpeg;base64,', '')).length;
+          dataUrl = canvas.toDataURL("image/jpeg", q);
+          encUrl = atob(dataUrl.replace("data:image/jpeg;base64,", "")).length;
 
           if (encUrl > origin) {
             q -= d /= 2;
@@ -86,13 +92,19 @@ export default class Resize {
         }
 
         return resolve(dataUrl);
-      }
+      };
     });
   }
 
   urlToBlob(result, fileName) {
-    const bytes = result.split(',')[0].indexOf('base64') >= 0 ? atob(result.split(',')[1]) : encodeURI(result.split(',')[1]);
-    const mime = result.split(',')[0].split(':')[1].split(';')[0];
+    const bytes =
+      result.split(",")[0].indexOf("base64") >= 0
+        ? atob(result.split(",")[1])
+        : encodeURI(result.split(",")[1]);
+    const mime = result
+      .split(",")[0]
+      .split(":")[1]
+      .split(";")[0];
     const max = bytes.length;
     const ia = new Uint8Array(max);
 
@@ -100,6 +112,8 @@ export default class Resize {
       ia[i] = bytes.charCodeAt(i);
     }
 
-    return new File([new Blob([ia], { type: mime })], fileName);
+    console.log('type : ', mime)
+
+    return new File([new Blob([ia], { type: mime })], fileName, { type: mime });
   }
-};
+}
